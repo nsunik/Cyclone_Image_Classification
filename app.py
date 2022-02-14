a519fa8d6e20a753307d8d6955e3775b45e23fd4
N.Sunik <71277942+nsunik@users.noreply.github.com>
2022-02-14 18:22:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ with c1:
                 st.markdown("This Image most likely belongs to")
                 st.subheader(
                     " {} with a {:.2f} percent confidence."
-                        .format(class_names[np.argmax(results)]))
+                        .format(class_names[np.argmax(results),np.max(results)]))
                 #st.write(results)#100 * ,np.max(results)
         
 
