c4ff38d23aa39e3a54d85c3a345d97f0cc832993
N.Sunik <71277942+nsunik@users.noreply.github.com>
2022-02-16 10:29:51 +0530

    Update classify.py

--- classify.py
+++ classify.py
@@ -9,12 +9,12 @@ import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
 def get_model():
-    json_file = open('classifier.json', 'r')
+    json_file = open('classifier1.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
     # load weights into new model
-    loaded_model.load_weights("weights.hdf5")
+    loaded_model.load_weights("weights (2).hdf5")
     return loaded_model
 
 
