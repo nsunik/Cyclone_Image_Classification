452589a7c201c3715637ad234c936a847b100dbe
N.Sunik <71277942+nsunik@users.noreply.github.com>
2022-02-23 21:27:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ with c1:
                 st.subheader(
                     " {} with a {:.2f} percent confidence."
                         .format(class_names[np.argmax(results)], 100 * np.max(results)))
-                st.write(results)
+                #st.write(results), 100 * np.max(results)
                
         
 
